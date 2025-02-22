45ecde70642a1a7979c9ac443de008d0538c261d
Nishank1705 <nishothers@gmail.com>
2025-02-22 10:04:27 +0530

    [Nishank Bathri] Add. UC2_GET_NAME

---
+++ api/src/main/java/com/myFirst/api/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.myFirst.api;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping(value = {"/uc1", "/hello"})
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    //UC2 Request parameter
+    @GetMapping("/input")
+    public String sayHello(@RequestParam(defaultValue = "Mark") String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
