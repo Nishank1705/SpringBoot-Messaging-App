62cdf5dd9f1732858c6e794a7df5232d1b235570
Nishank1705 <nishothers@gmail.com>
2025-02-22 10:17:41 +0530

    [Nishank Bathri] Add. UC5_Put_Request

---
+++ api/src/main/java/com/myFirst/api/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.myFirst.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping(value = {"/uc1", "/hello"})
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    //UC2 Request parameter
+    @GetMapping("/input")
+    public String sayHello(@RequestParam(defaultValue = "Mark") String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    //uc3 Parameter using path variable
+    @GetMapping("/query/{name}")
+    public String display(@PathVariable String name){
+        return "Hello "+ name+ " from BridgeLabz";
+    }
+
+    // uc4 getting first and last name using post request
+    @PostMapping("/getfullname")
+    public String sayHello(@RequestBody DtoUser user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+    // uc5 getting firstname as path variable and lastname as query parameter in put method
+    @PutMapping("/hello/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz!";
+    }
+}
